38d4dc59606512f9a3028445e8fa519771b1ddf9
Ebe <ebesetiobhio@gmail.com>
2025-02-03 21:35:52 +0000

    added readme instructions

---
+++ task-list-app/jest.config.js
@@ -0,0 +1,15 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
+};
+
+module.exports = createJestConfig(customJestConfig);
